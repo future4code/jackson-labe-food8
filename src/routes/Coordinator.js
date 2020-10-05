1d5cffb910b9bcc067eb2a67de163b4371c49c93
Lucas  Duarte <dolucasduarte@gmail.com>
2020-10-05 15:01:02 -0300

    Instalacao do projeto

---
+++ src/routes/Coordinator.js
@@ -0,0 +1,28 @@
+export const goToLogin = (history) => {
+    history.push('/login')
+  }
+  
+  export const goToSignUp = (history) => {
+    history.push('/signup')
+  }
+  
+  export const goToFeed = (history) => {
+    history.push('/')
+  }
+  
+  export const goToRestaurant = (history, id) => {
+    history.push(`/restaurants/${id}`)
+  }
+
+  export const goToShoppingCart = (history) => {
+    history.push(`/shoppingcart`)
+  }
+
+  export const goToProfile = (history) => {
+    history.push(`/profile`)
+  }
+
+  export const goBack = (history) => {
+    history.goBack()
+  }
+  
---
+++ src/screens/Restaurant/RestaurantPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantPage = () => {
+    return (
+        <div>
+            pagina do restaurante
+        </div>
+    )
+}
+
+export default RestaurantPage
