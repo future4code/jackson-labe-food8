7c7f02254b2089cacb370e5f29ff8546853b8e72
Lucas  Duarte <dolucasduarte@gmail.com>
2020-10-08 10:19:35 -0300

    Header no cadastro

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -30,9 +30,11 @@ const Router = () => {
                 <LoginPage/> 
             </Route>
             <Route exact path='/signup'>
+                <Header back={true}/>
                 <SignUpPage/> 
             </Route>
             <Route exact path='/address'>
+                <Header back={true}/>
                 <AddressPage/> 
             </Route>
             <Route exact path='/restaurants/:id'>
